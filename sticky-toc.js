25344236cbb301afde40c1cd5098d9bb5c1dac3a
Mátyás Budavári <budavariam@gmail.com>
2024-08-27 12:13:07 +0200

    Wait until content is loaded

--- sticky-toc.js
+++ sticky-toc.js
@@ -1,43 +1,41 @@
-const aa = document.createElement("aside")
-document.body.appendChild(aa)
-
-const headings = Array.from(document.getElementsByTagName("h2"));
-const aside = document.querySelector("aside");
-const ul = document.createElement("ul");
-aside.appendChild(ul);
-headings.map((heading) => {
-    const id = heading.innerText.toLowerCase().replaceAll(" ", "_");
-    heading.setAttribute("id", id);
-    const anchorElement = `<a href="#${id}">${heading.textContent}</a>`;
-    const keyPointer = `<li>${anchorElement}</li>`;
-    ul.insertAdjacentHTML("beforeend", keyPointer);
-});
-const tocAnchors = aside.querySelectorAll("a");
-const obFunc = (entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            const index = headings.indexOf(entry.target);
-            tocAnchors.forEach((tab) => {
-                tab.classList.remove("active");
-            });
-            tocAnchors[index].classList.add("active");
-            tocAnchors[index].scrollIntoView({
-                block: "nearest",
-                inline: "nearest"
-            });
-        }
+window.addEventListener("DOMContentLoaded", (event) => {
+    const aside = document.createElement("aside")
+    const headings = Array.from(document.getElementsByTagName("h2"));
+    const ul = document.createElement("ul");
+    aside.appendChild(ul);
+    headings.map((heading) => {
+        const id = heading.innerText.toLowerCase().replaceAll(" ", "_");
+        heading.setAttribute("id", id);
+        const anchorElement = `<a href="#${id}">${heading.textContent}</a>`;
+        const keyPointer = `<li>${anchorElement}</li>`;
+        ul.insertAdjacentHTML("beforeend", keyPointer);
     });
-};
-const obOption = {
-    rootMargin: "-30px 0% -77%",
-    threshold: 1
-};
-const observer = new IntersectionObserver(obFunc, obOption);
-headings.forEach((hTwo) => observer.observe(hTwo));
+    const tocAnchors = aside.querySelectorAll("a");
+    const obFunc = (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const index = headings.indexOf(entry.target);
+                tocAnchors.forEach((tab) => {
+                    tab.classList.remove("active");
+                });
+                tocAnchors[index].classList.add("active");
+                tocAnchors[index].scrollIntoView({
+                    block: "nearest",
+                    inline: "nearest"
+                });
+            }
+        });
+    };
+    const obOption = {
+        rootMargin: "-30px 0% -77%",
+        threshold: 1
+    };
+    const observer = new IntersectionObserver(obFunc, obOption);
+    headings.forEach((hTwo) => observer.observe(hTwo));
 
 
-const style = document.createElement('style');
-style.innerHTML = `
+    const style = document.createElement('style');
+    style.innerHTML = `
 body {
     padding: 10px;
     display: grid;
@@ -83,4 +81,6 @@ aside ul li a.active {
     }
   }
 `;
-document.body.appendChild(style)
+    document.body.appendChild(style)
+    document.body.appendChild(aside)
+})
